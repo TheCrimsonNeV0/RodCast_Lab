d0277c994d30587bd88cdc32e0bc7c27fc9d4824
Nevzat Umut Demirseren <60689329+TheCrimsonNeV0@users.noreply.github.com>
2025-03-05 17:04:16 -0500

    Go-go hand implementation
    Go-go hand is implemented. The technique can be selected from the menu.

--- Assets/Scripts/TechniqueSelectionHandlerScript.cs
+++ Assets/Scripts/TechniqueSelectionHandlerScript.cs
@@ -6,16 +6,26 @@ public class TechniqueSelectionHandlerScript : MonoBehaviour
 {
     public GameObject rodcastInteraction;
     public GameObject flowerConeInteraction;
+    public GameObject goGoHandInteraction;
 
     public void OnButtonClickRodCast()
     {
         flowerConeInteraction.SetActive(false);
+        goGoHandInteraction.SetActive(false);
         rodcastInteraction.SetActive(true);
     }
 
     public void OnButtonClickFlowerCone()
     {
         rodcastInteraction.SetActive(false);
+        goGoHandInteraction.SetActive(false);
         flowerConeInteraction.SetActive(true);
+    }
+
+    public void OnButtonClickGoGoHand()
+    {
+        rodcastInteraction.SetActive(false);
+        flowerConeInteraction.SetActive(false);
+        goGoHandInteraction.SetActive(true);
     }
 }
